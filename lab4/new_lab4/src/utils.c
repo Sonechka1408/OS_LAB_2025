e26673be3da4a5c01e152478297f4bfe4a19df5d
Sonechka1408 <molochnayakorovka@gmail.com>
2025-11-28 19:35:49 +0000

    Lab4, 5, 6

--- lab4/new_lab4/src/utils.c
+++ lab4/new_lab4/src/utils.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   srand(seed);
   for (unsigned int i = 0; i < array_size; i++) {
     array[i] = rand();
   }
 }
+
 
 
